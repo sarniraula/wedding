6d234f8b1fba809911e290b2789486034ecb1f39
sarniraula <sar.niraula162@gmail.com>
2024-10-28 11:28:36 -0400

    Resize image sizes.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 import { FaHeart } from 'react-icons/fa';
-import gallery1 from '../assets/gallery1.jpeg';
-import gallery2 from '../assets/gallery2.jpeg';
-import gallery3 from '../assets/gallery3.jpeg';
-import gallery4 from '../assets/gallery4.jpeg';
-import gallery5 from '../assets/gallery5.jpeg';
-import gallery6 from '../assets/gallery6.jpeg';
-import gallery7 from '../assets/gallery7.jpeg';
-import gallery8 from '../assets/gallery8.jpeg';
-import gallery9 from '../assets/gallery9.jpeg';
-import gallery10 from '../assets/gallery10.jpeg';
-import gallery11 from '../assets/gallery11.jpeg';
-import gallery12 from '../assets/gallery12.jpeg';
-import gallery13 from '../assets/gallery13.jpeg';
-import gallery14 from '../assets/gallery14.jpeg';
-import gallery15 from '../assets/gallery15.jpeg';
-import gallery16 from '../assets/gallery16.jpeg';
-import gallery17 from '../assets/gallery17.jpeg';
-import gallery18 from '../assets/gallery18.jpeg';
-import gallery19 from '../assets/gallery19.jpeg';
-import gallery20 from '../assets/gallery20.jpeg';
-import gallery21 from '../assets/gallery21.jpeg';
-import gallery22 from '../assets/gallery22.jpeg';
-import gallery23 from '../assets/gallery23.jpeg';
-import gallery24 from '../assets/gallery24.jpeg';
-import gallery25 from '../assets/gallery25.jpeg';
-import gallery26 from '../assets/gallery26.jpeg';
-import gallery27 from '../assets/gallery27.jpeg';
-import gallery28 from '../assets/gallery28.jpeg';
-import gallery29 from '../assets/gallery29.jpeg';
+import gallery1 from '../assets/gallery1.jpg';
+import gallery2 from '../assets/gallery2.jpg';
+import gallery3 from '../assets/gallery3.jpg';
+import gallery4 from '../assets/gallery4.jpg';
+import gallery5 from '../assets/gallery5.jpg';
+import gallery6 from '../assets/gallery6.jpg';
+import gallery7 from '../assets/gallery7.jpg';
+import gallery8 from '../assets/gallery8.jpg';
+import gallery9 from '../assets/gallery9.jpg';
+import gallery10 from '../assets/gallery10.jpg';
+import gallery11 from '../assets/gallery11.jpg';
+import gallery12 from '../assets/gallery12.jpg';
+import gallery13 from '../assets/gallery13.jpg';
+import gallery14 from '../assets/gallery14.jpg';
+import gallery15 from '../assets/gallery15.jpg';
+import gallery16 from '../assets/gallery16.jpg';
+import gallery17 from '../assets/gallery17.jpg';
+import gallery18 from '../assets/gallery18.jpg';
+import gallery19 from '../assets/gallery19.jpg';
+import gallery20 from '../assets/gallery20.jpg';
+import gallery21 from '../assets/gallery21.jpg';
+import gallery22 from '../assets/gallery22.jpg';
+import gallery23 from '../assets/gallery23.jpg';
+import gallery24 from '../assets/gallery24.jpg';
+import gallery25 from '../assets/gallery25.jpg';
+import gallery26 from '../assets/gallery26.jpg';
+import gallery27 from '../assets/gallery27.jpg';
+import gallery28 from '../assets/gallery28.jpg';
+import gallery29 from '../assets/gallery29.jpg';
 
 const title = 'Prenup Photos';
 
@@ -85,9 +85,9 @@ const Gallery = () => {
         </h2>
       </div>
 
-      <div className='flex items-center gap-3 mb-10 justify-center'>
+      <div className='flex items-center gap-3 mb-24 justify-center'>
         <div className="h-1 w-16 bg-emerald-600"></div>
-          <FaHeart className="text-rose-600" size={20} />
+          <FaHeart className="text-emerald-600" size={20} />
         <div className="h-1 w-16 bg-emerald-600"></div>
       </div>
 
