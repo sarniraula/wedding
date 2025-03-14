215c42e0e5591c27debdef2da8d91b3b6cf82456
sarniraula <sar.niraula162@gmail.com>
2025-03-13 20:58:34 -0400

    Remove dress code section

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <Home />
       <Events />
         <Map />
-        <DressCode />
+        {/* <DressCode /> */}
       <Invitation />
       <Ourstory />
       <Countdown />
